14675550be72fa140c95e5eb5cae8dd9674bc335
Suman Sigdel <53033648+sumansid@users.noreply.github.com>
2021-02-18 16:29:54 -0500

    remove comments for clarity

--- app.js
+++ app.js
@@ -16,22 +16,14 @@ app.get('/', (req, res) => {
 var socket = require("socket.io");
 var io = socket(server);
 io.sockets.on('connection', function(client){
-	//console.log('socket id', socket.id);
+
 	client.on('mouse', message);
-	
 	client.on('createNewRoom',handleNewRoom)
 	client.on('joinRoom', handleJoinRoom);
 
 	function message(data){
-		//console.log("room lookup", roomLookup[client.id])
-
-		const currRoom = io.sockets.adapter.rooms[roomLookup[client.id]];
-
-
 		console.log('current room', currRoom);
-		//client.broadcast.emit("mouse", data);
 		client.to(roomLookup[client.id]).emit("mouse", data);
-		//io.to(roomLookup[client.id]).emit("mouse", data);
 		console.log("data received");
 	}
 
@@ -45,30 +37,14 @@ io.sockets.on('connection', function(client){
 		client.emit("roomCode", roomID);
 		client.emit("init");
 
-
 	}
 
 	function handleJoinRoom(roomID){
 		console.log('trying to join ', roomID);
-		const currRoom = io.sockets.adapter.rooms[roomID];
-		let allUsers ;
-		if (currRoom){
-			allUsers = currRoom.sockets;
-
-		}
-		let numberOfClients;
-		if (allUsers){
-			console.log("ALL users", allUsers);
-			numberOfClients = Object.keys(allUsers).length;
-			console.log("Number of users in the room", numberOfClients);
-
-		}
-
-		console.log('Join room function triggered to join room'+ roomID);
 		roomLookup[client.id] = roomID;
-		console.log("roomLookup",roomLookup);
 		client.join(roomID);
 		client.emit("init");
+		client.emit("displayCurrentRoom", roomID);
 
 	}
 
