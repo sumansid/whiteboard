c51f79eae3e2f2a9f7ec6908065d5c1780c21ff2
Suman Sigdel <53033648+sumansid@users.noreply.github.com>
2021-02-18 16:17:20 -0500

    refactor code, emit init upon joining room

--- app.js
+++ app.js
@@ -18,29 +18,57 @@ var io = socket(server);
 io.sockets.on('connection', function(client){
 	//console.log('socket id', socket.id);
 	client.on('mouse', message);
-	//socket.on('newWhiteBoard', handleNewBoard);
+	
 	client.on('createNewRoom',handleNewRoom)
 	client.on('joinRoom', handleJoinRoom);
 
 	function message(data){
-		client.broadcast.emit("mouse", data);
-		console.log("data received",data);
+		//console.log("room lookup", roomLookup[client.id])
+
+		const currRoom = io.sockets.adapter.rooms[roomLookup[client.id]];
+
+
+		console.log('current room', currRoom);
+		//client.broadcast.emit("mouse", data);
+		client.to(roomLookup[client.id]).emit("mouse", data);
+		//io.to(roomLookup[client.id]).emit("mouse", data);
+		console.log("data received");
 	}
 
 	function handleNewRoom(){
 
 		var roomID = makeNewID();
-		roomLookup[socket.id] = roomID;
-		console.log("New room id was made", roomID);
+		roomLookup[client.id] = roomID;
+		console.log("New room id was made ", roomID);
 		client.join(roomID);
-		console.log('client emitted roomdcode', roomID);
+		console.log('client emitted room code ', roomID);
 		client.emit("roomCode", roomID);
+		client.emit("init");
+
 
 	}
 
 	function handleJoinRoom(roomID){
+		console.log('trying to join ', roomID);
+		const currRoom = io.sockets.adapter.rooms[roomID];
+		let allUsers ;
+		if (currRoom){
+			allUsers = currRoom.sockets;
+
+		}
+		let numberOfClients;
+		if (allUsers){
+			console.log("ALL users", allUsers);
+			numberOfClients = Object.keys(allUsers).length;
+			console.log("Number of users in the room", numberOfClients);
+
+		}
+
 		console.log('Join room function triggered to join room'+ roomID);
+		roomLookup[client.id] = roomID;
+		console.log("roomLookup",roomLookup);
 		client.join(roomID);
+		client.emit("init");
 
 	}
 
